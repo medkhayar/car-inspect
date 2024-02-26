7abc8bd104d0ff12bfad6986ce200b214b6e2adb
medkhayar <med.khayar.87@gmail.com>
2024-02-26 17:45:41 +0100

    Update page.tsx

--- src/app/access/page.tsx
+++ src/app/access/page.tsx
@@ -36,7 +36,7 @@ export default function AccessPage(){
             >
             <div className="absolute bg-gradient-to-b from-picton-blue-600 to-picton-blue-400 opacity-75 inset-0 z-0"></div>
             <div className="w-full  max-w-md z-10">
-              <div className="sm:text-4xl xl:text-5xl font-bold leading-tight mb-6">Reference site about Lorem Ipsum..</div>
+              <div className="sm:text-4xl xl:text-5xl font-bold leading-tight mb-6">Car Inspect.</div>
               <div className="sm:text-sm xl:text-md text-gray-200 font-normal"> What is Lorem Ipsum Lorem Ipsum is simply dummy
                 text of the printing and typesetting industry Lorem Ipsum has been the industry's standard dummy text ever
                 since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book it
